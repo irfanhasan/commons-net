8ff51072915e1ec2a5101635865de03a52bc0dd1
irfanhasan <ihasan03@gmail.com>
2017-10-11 12:37:39 -0500

    travisCI and ekstazi integration

---
+++ pull.sh
@@ -0,0 +1,3 @@
+git clone https://${GH_TOKEN}@github.com/irfanhasan/commons-net-cache.git 
+cp -R travisCIcache/.ekstazi/ .ekstazi/
+rm -rf travisCIcache
---
+++ push.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+setup_git() {
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis CI"
+    mkdir temp
+}
+
+commit_files() {
+    cd temp
+    git init
+    cp -R ../.ekstazi/ .ekstazi/
+    git add -f .ekstazi/*
+    git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+    git remote add origin https://${GH_TOKEN}@github.com/irfanhasan/commons-net-cache.git 
+    git push origin master
+}
+
+setup_git
+commit_files
+upload_files
